784a8aa59f19f8dc1b1b8735c466795ed7cbc78a
hayashahid13dec <hayashahid13dec@gmail.com>
2023-06-15 15:52:57 +0500

    completed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tiktok_flutter/main.dart';
+import 'package:tiktok_flutter/screens/feed_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( FeedScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
